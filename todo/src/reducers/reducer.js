2062c2135811828a322d6cbfc1be559e0cb871bf
Pete1701 <pete.pertongonas@pirron.one>
2020-03-21 23:38:26 +0100

    its working1

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,10 +1,10 @@
 export const initialState = {
   items: [
-      {
-        item: 'Learn about reducers',
-        completed: false,
-        id: 3892987589
-      }
+    //   {
+    //     item: 'Learn about reducers',
+    //     completed: false,
+    //     id: 3892987589
+    //   }
   ]  
   };
 
